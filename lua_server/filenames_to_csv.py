363ae7010f811a23050df88567cba9402422827d
snes server <snes@snesio.com>
2016-03-17 21:17:13 -0400

    Sorted CSVs

--- lua_server/filenames_to_csv.py
+++ lua_server/filenames_to_csv.py
@@ -1,9 +1,13 @@
+lines = []
 print("frames,fitness")
 for line in open("filenames"):
     line = line.split()
     if len(line) > 8:
         filename = line[8].split("_")
         if len(filename) > 1:
-            fitness = filename[0]
-            frames = filename[1].split(".genome")[0]
-            print(frames + "," + fitness)
+            fitness = float(filename[0])
+            frames = int(filename[1].split(".genome")[0])
+            lines.append([frames, fitness])
+
+for line in sorted(lines, key=lambda x: x[0]):
+    print(str(line[0]) + "," + str(line[1]))
